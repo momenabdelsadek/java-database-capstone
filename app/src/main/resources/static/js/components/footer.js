79a8ba32c010420a1887f467b45a77c1e649c516
m.abdelsadek <m.abdelsadek@scbank.com.eg>
2025-07-13 18:30:30 +0300

    update logo url

--- app/src/main/resources/static/js/components/footer.js
+++ app/src/main/resources/static/js/components/footer.js
@@ -118,7 +118,7 @@ function renderFooter() {
 
         <!-- Branding Section -->
         <div class="footer-logo">
-          <img src="../assets/images/logo/logo.png" alt="Hospital CMS Logo">
+          <img src="./assets/images/logo/logo.png" alt="Hospital CMS Logo">
           <p>© Copyright 2025. All Rights Reserved by Hospital CMS.</p>
         </div>
 
